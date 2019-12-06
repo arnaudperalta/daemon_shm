64730a88a55ec04be9c2f9e31380f53ab7e5f9a9
Arnaud Peralta <arnaud.peralta@gmail.com>
2019-12-06 16:17:19 +0100

    Tubes/SHM ini

--- demon/demon.c
+++ demon/demon.c
@@ -5,15 +5,19 @@
 #include <sys/stat.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <string.h>
+#include "demon.h"
 #include "thread_manager.h"
 
-#define TUBE_IN        "../tubes/daemon_shm_in"
-#define CONFIG_PATH    "demon.conf"
-#define SHM_SEM_NAME   "/shm/shm_sem"
+#define CONFIG_PATH        "demon.conf"
 
-#define OPTIONS_NUMBER 4
-#define FUN_SUCCESS    0
-#define FUN_FAILURE    -1
+#define OPTIONS_NUMBER     4
+#define FUN_SUCCESS        0
+#define FUN_FAILURE        -1
+#define MAX_COMMAND_LENGTH 100
+#define PID_LENGTH         5
+#define MES_LENGTH         4
+#define BUFFER_LENGTH      32
 
 typedef struct config {
 	size_t min_thread;
@@ -53,7 +57,7 @@ int main(void) {
 	if (mkfifo(TUBE_IN, S_IRUSR | S_IWUSR) == -1) {
 		perror("Erreur création tube demon");
 		exit(EXIT_FAILURE);
-	  }
+	}
 
 	if (unlink(TUBE_IN) == -1) {
       perror("Erreur suppression tube demon");
@@ -79,9 +83,15 @@ int main(void) {
 	}
 	
 	// Initilisation de MIN_THREAD threads
-	pthread_t *tab = ini_thread(cfg.min_thread, &(cfg.max_con_per_thread), shm_sem);
-	if (tab == NULL) {
+	pthread_m *ptr = ini_thread(cfg.min_thread, &(cfg.max_con_per_thread), &(cfg.max_thread), shm_sem);
+	if (ptr == NULL) {
 		perror("Erreur ini threads");
+		exit(EXIT_FAILURE);
+	}
+	
+	// Mise en écoute du démon auprès des clients
+	if (tube_listening(ptr) == -1) {
+		perror("Erreur tube_listening");
 		exit(EXIT_FAILURE);
 	}
 	
@@ -113,6 +123,73 @@ int load_config(config *cfg) {
 	return FUN_SUCCESS;
 }
 
-void tube_listening(void) {
+int tube_listening(config *cfg, thread_m *manager) {
+	// PID du client qui est en train d'établir une connection
+	char client_pid[PID_LENGTH + 1];
 	
+	// File descriptor du tube client, on ne connait pas encore son nom
+	int fd_out;
+	// File descriptor du demon
+	int fd_in = open(TUBE_IN, O_RDONLY);
+	if (fd == -1) {
+		perror("Erreur ouverture tube");
+		return EXIT_FAILURE;
+	}
+	
+	ssize_t n;
+	int thread_number;
+	while (1) {
+		// Les messages recu seront de la forme 00XXXSYNC\0 ou 00XXXEND\0 (x : pid)
+		char buffer_read[PID_LENGTH + MES_LENGTH + 1];
+		
+		// Les messages envoyés seront de la forme RST ou SHM_NAMEXXX (X : numero du thread)
+		char buffer_write[BUFFER_LENGTH];
+		
+		// Nom supposé du tube client
+		char tube_name[strlen(TUBE_OUT) + PID_LENGTH + 1];
+		strcat(tube_name, TUBE_OUT);
+		
+		while ((n = read(fd, buffer_read, sizeof buffer_read)) > 0 {
+			// Le client envoie SYNC avec son PID
+			if (strcmp(SYNC_MSG, buffer + PID_LENGTH) == 0) {
+				// Récupération d'un numero de thread libre puis on averti
+				//   le client du nom du shm a écouté.
+				sprintf(buffer_read, "%5s", client_pid);
+				strcat(tube_name, client_pid);
+				
+				// Ouverture du tube client
+				fd_out = open(tube_name, O_WRONLY);
+				if (fd_out == -1) {
+					perror("Erreur ouverture tube client");
+					return FUN_FAILURE;
+				}
+				
+				if (unlink(tube_name) == -1) {
+					perror("Unlink tube client");
+					return FUN_FAILURE;
+				}
+				
+				thread_number = use_thread(manager);
+				if (thread_number == -1) {
+					// Pas de thread dispo, on envoie RST
+					if (write(fd_out, RST_MSG, sizeof RST_MSG) == -1) {
+						perror("Erreur écriture tube client");
+						return FUN_FAILURE;
+					}
+				} else {
+					// Un thread est disponible, on envoie donc le nom du shm au client
+					sprintf(buffer_write, %s%d, SHM_NAME, thread_number);
+					if (write(fd_out, buffer_write, sizeof buffer_write) == -1) {
+						perror("Erreur écriture tube client");
+						return FUN_FAILURE;
+					}
+				}
+				if (close(fd_out) == -1) {
+					perror("Erreur close tube client");
+					return FUN_FAILURE;
+				}
+			}
+		}
+		printf("Client fini\n");
+	}
 }
--- demon/thread_manager/thread_manager.c
+++ demon/thread_manager/thread_manager.c
@@ -2,56 +2,95 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <pthread.h>
-
+#include "demon.h"
 #include "thread_manager.h"
 
-// Structure element stockant une paire thread / semaphore / connection restante
+#define SHM_SIZE     1024
+#define NEUTRAL_FLAG 0
+#define DEMON_FLAG   1
+#define CLIENT_FLAG  2
+
+// Structure element stockant un thread avec son shm, son mutex etc ...
 typedef struct thread_e {
 	pthread_t thread;
 	pthread_mutex_t mutex;
 	size_t con_left;
+	int shm_fd;
+	volatile int *rdv;  // 0 de base, 1 pour une donnée reservé au demon, 2 pour le client
 } thread_e;
 
-// Structure principale stockant chaque paire thread/semaphore
+// Structure principale stockant les threads_e
 struct thread_m {
-	thread_e *array;
-	size_t count;
+	thread_e *array;   // Tableau de thread_e de taille max_thread
+	size_t count;      // Nombre de thread vivant
+	size_t max_thread; // Nombre de thread maximal
 } thread_m;
 
-thread_e *ini_thread_element(size_t max_con) {
-	thread_e ptr
-	pthread_t th;
-	ptr->thread = th;
-	pthread_mutex_t mu;
-	if (pthread_mutex_init(&mu, NULL) != 0) {
+thread_e *ini_thread_element(size_t number, size_t max_con) {
+	thread_e *ptr = malloc(sizeof (thread_e));
+	if (ptr == NULL) {
+		return NULL;
+	}
+	if (pthread_mutex_init(&(ptr->mutex), NULL) != 0) {
 		perror("Error pthread mutex");
 		return NULL;
 	}
-	ptr->mutex = mu;
+	pthread_create(ptr->thread, NULL, waiting_command, ptr);
 	ptr->con_left = max_con;
 	
-	pthread_create(ptr->thread, NULL, waiting_command, ptr);
+	char shmname[32];
+	sprintf(shmname, "%s%d", SHM_NAME, number);
+	
+	ptr->shm_fd = shm_open(shmname, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (ptr->shm_fd == -1) {
+		perror("Erreur création shm");
+		return NULL;
+	}
+	
+	if (shm_unlink(shmname) == -1) {
+		perror("Erreur unlink shm");
+		return NULL;
+	}
+	
+	// On fixe la taille de notre shm. SHM_SIZE
+	if (ftruncate(ptr->shm_fd, SHM_SIZE) == -1) {
+		perror("Erreur truncate shm thread");
+		exit(EXIT_FAILURE);
+	}
+	
+	ptr->rdv = mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, ptr->shm_fd, 0);
+	if (ptr->rdv == MAP_FAILED) {
+		perror("Erreur map");
+		return NULL;
+	}
+	
+	// Initialisation du point de rendez-vous
+	*(ptr->rdv) = NEUTRAL_FLAG;
+	
 	return ptr;
 }
 
 
-thread_m *ini_thread(size_t count, size_t max_con, int shm_sem) {
+thread_m *ini_thread(size_t min_thread,PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0); size_t max_con, size_t max_thread, int shm_sem) {
 	// Allocation de la structure thread_m
 	pthread_m *ptr = malloc(sizeof (thread_m));
 	if (ptr == NULL) {
 		return NULL;
 	}
-	ptr->count = count;
+	ptr->count = min_thread;
 	
 	// Allocation du tableau de pointeur de thread_e
-	ptr->array = malloc(sizeof (thread_e) * count);
+	ptr->array = malloc(sizeof (thread_e) * max_thread);
 	if (ptr->array == NULL) {
 		return NULL
 	}
 	
 	// Allocation de "count" threads
 	for (int i = 0; i < count; ++i) {
-		ptr->array[i] = ini_thread_element(max_con);
+		ptr->array[i] = ini_thread_element(i, max_con);
+		if (ptr->array[i] == NULL) {
+			return NULL;
+		}
 	}
 	
 	return ptr;
@@ -59,11 +98,44 @@ thread_m *ini_thread(size_t count, size_t max_con, int shm_sem) {
 
 void *waiting_command(void *arg) {
 	thread_e *ptr = (thread_e *) arg;
+	// Pointeur de fichier contenant le stdout de la commande a executé
+	FILE *res;
+	size_t command_length = SHM_SIZE - sizeof (int);
+	char command[command_length];
+	
 	// Lecture d'une commande
+	while (*(ptr->rdv) != DEMON_FLAG);
+	sprintf(command, %s, (char *) (ptr->rdv + sizeof (int)));
 	
-	// Execution
+	// On lock le thread
 	pthread_mutex_lock(ptr->mutex);
+	// Execution
+	res = popen(command, "r");
 	
+	// Envoi du résultat
+	fgets((char *) (ptr->rdv + sizeof (int)), command_length, res);
+	*(ptr->rdv) = 2;
+	
+	// On unlock le thread
 	pthread_mutex_unlock(ptr->mutex);
 	return NULL;
+}
+
+int use_thread(thread_m *th, size_t max_con) {
+	for (int i = 0; i < th->count; ++i) {
+		// On verifie si ce thread est libre d'utilsation
+		if (pthread_mutex_trylock(th->array[i]->mutex) == 0) {
+			pthread_mutex_unlock(th->array[i]->mutex);
+			return i;
+		}
+	}
+	
+	// Pas de thread dispo, on regarde si on a atteint le max
+	if (th->count < th->max_thread) {
+		th->array[th->count] = ini_thread_element(th->count, max_con);
+		++th->count;
+		return th->count - 1;
+	}
+	
+	return -1;
 }
